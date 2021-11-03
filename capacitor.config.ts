c0dec58e284380cd6540c8bed81946580df5e3d9
ianrose2k <ianrose2k@gmail.com>
2021-11-03 14:55:29 -0700

    new app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CFGI APP Test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

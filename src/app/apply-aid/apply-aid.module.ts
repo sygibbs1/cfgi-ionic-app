0252f8d4ea30c9d7f5925c3ea32a235bbeecd112
ianrose2k <ianrose2k@gmail.com>
2021-12-02 19:38:27 -0800

    Update aid application page

--- src/app/apply-aid/apply-aid.module.ts
+++ src/app/apply-aid/apply-aid.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { ApplyAidPage } from './apply-aid.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ApplyAidPageRoutingModule
+    ApplyAidPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [ApplyAidPage]
 })
--- src/app/apply-aid/apply-aid.page.ts
+++ src/app/apply-aid/apply-aid.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-apply-aid',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplyAidPage implements OnInit {
 
+  private formData: FormGroup
+
+
   constructor() { }
 
   ngOnInit() {
+
+    this.formData = new FormGroup({
+      firstName: new FormControl(),
+      familyName: new FormControl(),
+      email: new FormControl(),
+      phone: new FormControl(),
+      dateOfBirth: new FormControl(),
+      whyInternational: new FormControl(),
+      degreeLevel: new FormControl(),
+      college: new FormControl(),
+      major: new FormControl(),
+      year: new FormControl(),
+      maritalStatus: new FormControl(),
+      children: new FormControl(),
+      amount: new FormControl(),
+      why: new FormControl(),
+      immigrationStatus: new FormControl(),
+      familyHardship: new FormControl(),
+      finDocs: new FormControl(),
+    })
+  }
+
+  onSubmit() {
+    console.log(this.formData.value);
   }
 
 }

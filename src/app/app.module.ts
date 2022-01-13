078036f1afabf0145a592ec23a5160871f73b39f
ianrose2k <ianrose2k@gmail.com>
2022-01-13 15:18:24 -0800

    Implement Swiper.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { SwiperModule } from 'swiper/angular';
 
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -11,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SwiperModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swiper, {SwiperOptions, Pagination} from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  
+export class HomePage implements OnInit{
+  public swiperConfig: SwiperOptions = {
+    pagination: true
+  }
   constructor() {}
 
+  ngOnInit() {
+    Swiper.use([Pagination])
+  }
 }
 
